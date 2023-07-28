339df242946481e4abdd947a744169994881ecd8
TheTibo <140638655+TheTibo@users.noreply.github.com>
2023-07-28 22:23:33 +0200

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -18,13 +18,5 @@ myImage.addEventListener("click", function () {
   }
 });
 
-let myImage = document.querySelector("img");
 
-myImage.addEventListener("click", function () {
-  let mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/paris1.jpg") {
-    myImage.setAttribute("src", "images/paris1.jpg");
-  } else {
-    myImage.setAttribute("src", "images/paris1.jpg");
-  }});
 
